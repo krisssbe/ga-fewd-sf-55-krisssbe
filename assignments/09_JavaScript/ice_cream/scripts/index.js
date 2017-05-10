5ea7c086d94cd51e6edd84ead556823ed058862f
krisssbe <kristin.bredemeier@gmail.com>
2017-05-09 18:07:17 -0700

    Changed m4a folder

--- assignments/09_JavaScript/ice_cream/scripts/index.js
+++ assignments/09_JavaScript/ice_cream/scripts/index.js
@@ -1,26 +1,25 @@
 
 console.log('Get ready for some ice-cream!');
 
-//make Cone function definition
-var makeCone = function (flavor, numberScoops) {
-  console.log('Coming right up! ' + numberScoops + ' scoops of ' + flavor + ' ice cream cone with ')
-} 
-
-var makeCone = function (flavor, numberScoops) {
-  console.log('Coming right up! ' + numberScoops + ' scoops of ' + flavor + ' ice cream cone with ')
-} 
-
-//make Cone function call
-makeCone('chocolate', 3);
-
-
 var flavor
 var numScoops
 
-on click flavor 
-  set flavor
-on click numScoops
-  set numScoops
+document.getElementById('#flavor').click();
+document.getElementById('#scoops').click();
 
-find order details placeholder
-update uts contents with flavor + numScoops
+function setflavor () {
+document.getElementByClass("flavor").console.log()  
+}
+
+function setscoops () {
+document.getElementByClass("scoops").console.log()  
+}
+
+
+//document.getElementById("flavor").addEventListener("click", function() {
+//    gotoNode(result.name);
+//}, false);
+
+
+
+
